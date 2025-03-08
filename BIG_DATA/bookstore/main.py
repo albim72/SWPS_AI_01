59cb3c375fb4a796a2c8d2d16e60131e09a96de0
albim72 <marcin.albiniak72@gmail.com>
2025-03-08 11:26:55 +0100

    Update main.py

--- BIG_DATA/bookstore/main.py
+++ BIG_DATA/bookstore/main.py
@@ -1,4 +1,5 @@
 from book import Book
+# import book
 
 bk = Book(3,"Wiedźmin","Andrzej Sapkowski",42)
 print(bk)
@@ -7,3 +8,8 @@ bk(21)
 
 bk.setid(88)
 print(bk.getid())
+
+print(bk.cena)
+
+bk.cena = 61
+print(bk.cena)
