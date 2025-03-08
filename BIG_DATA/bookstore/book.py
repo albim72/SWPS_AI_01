00970abbe53028102f5d44a8944780773c3880ae
albim72 <marcin.albiniak72@gmail.com>
2025-03-08 10:57:31 +0100

    Create book.py

---
+++ BIG_DATA/bookstore/book.py
@@ -0,0 +1,9 @@
+class Book:
+    def __init__(self,id,tytul,autor,cena=20):
+        self.idbook = id
+        self.tytul = tytul
+        self.autor = autor
+        self.cena = cena
+        self.oprawa = "miękka"
+
+        
