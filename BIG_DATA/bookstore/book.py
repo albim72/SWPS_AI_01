b9aaed574b5aa80cb592281b9e8b1e77056a138c
albim72 <marcin.albiniak72@gmail.com>
2025-03-08 11:06:55 +0100

    Update book.py

--- BIG_DATA/bookstore/book.py
+++ BIG_DATA/bookstore/book.py
@@ -1,9 +1,18 @@
 class Book:
+    #definicja stanu
+
+    # def __new__(cls, *args, **kwargs):
+    #     return object.__new__(Book)
+
     def __init__(self,id,tytul,autor,cena=20):
         self.idbook = id
         self.tytul = tytul
         self.autor = autor
         self.cena = cena
         self.oprawa = "miękka"
+        self.create_book()
 
-        
+    #definicja zachowania
+    def create_book(self):
+        print(f"utworzono ksiązkę - nowy obiekt klasy {self.__class__.__qualname__}")
+
