6f9b53de8169a2fbe4c234720e83b5b3c5e17bdf
albim72 <marcin.albiniak72@gmail.com>
2025-03-09 10:01:03 +0100

    Update simplenn.py

--- NEURAL_NETWORK/SimpleNN_project/simpleneurallib/simplenn.py
+++ NEURAL_NETWORK/SimpleNN_project/simpleneurallib/simplenn.py
@@ -8,3 +8,24 @@ class SimpleNeuralNetwork:
     def __repr__(self):
         return (f"nowa sieć neuronowa oparta na klasie: {self.__class__.__name__}\n"
                 f"Wylosowano wagi:\n{self.weights}\n")
+
+    #funkcja aktywacji - sigmoid
+    def sigmoid(self,x):
+        return 1/(1+np.exp(-x))
+
+    #różniczka funkcji aktywacji
+    def d_sigmoid(self,x):
+        return x*(1-x)
+
+    #funkcja propagacji
+    def propagation(self,inputs):
+        return self.sigmoid(np.dot(inputs.astype(float),self.weights))
+
+
+    #funkcja propagacji wstecznej
+    def backward_propagation(self,propagation_result,train_input,train_output):
+        error = train_output - propagation_result
+        self.weights += np.dot(train_input.T,error*self.d_sigmoid(propagation_result))
+
+
+
